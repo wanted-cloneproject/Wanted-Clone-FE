6d3dad3785903b0a0d628fa9dbf11075d69e7635
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-06-17 01:41:23 +0900

    ♻️ [refactor]: 인가코드 보내기

--- src/KakaoCallback.js
+++ src/KakaoCallback.js
@@ -13,55 +13,68 @@ const KakaoCallback = () => {
   useEffect(() => {
     // 1. 인가코드 전달하여 토큰 발급받기
     axios
-      .post(
-        `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${REDIRECT_URI}&code=${code}`,
-        {
-          headers: {
-            "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-          },
-        }
-      )
+      .post("https://wantedclone.com/login/", {
+        params: {
+          code: code,
+        },
+      })
       .then((res) => {
-        const access_token = res.data.access_token;
-
-        if (access_token) {
-          // 토큰 저장
-          localStorage.setItem("token", access_token);
-
-          // 2. 사용자 정보 받아오기
-          // axios
-          //   .post(
-          //     "https://kapi.kakao.com/v2/user/me",
-          //     {},
-          //     {
-          //       headers: {
-          //         Authorization: `Bearer ${access_token}`,
-          //       },
-          //     }
-          //   )
-          //   .then((res) => {
-          //     console.log("Kakao 사용자 정보 가져오기 성공!");
-          //     console.log(`닉네임: ${res.data.properties.nickname}`);
-          //     console.log(`이메일: ${res.data.kakao_account.email}`);
-          //     localStorage.setItem("socialUser", res.data.properties.nickname);
-          //   });
-
-          // 2. 백엔드로 토큰 보내기
-          axios
-            .get("https://wantedclone.com/login/", {
-              params: {
-                code: access_token,
-              },
-            }) // 사용자 정보 받기
-            .then((res) => {
-              // const nickName = res.nickName;
-              // const email = res.email;
-
-              console.log(res);
-              navigate("/");
-            });
-        }
+        console.log(res);
       });
+
+    // console.log(code);
+    // axios
+    //   .post(
+    //     `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+    //     {
+    //       headers: {
+    //         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    //       },
+    //     }
+    //   )
+    //   .then((res) => {
+    //     const access_token = res.data.access_token;
+
+    //     console.log(access_token);
+
+    //     if (access_token) {
+    //       // 토큰 저장
+    //       localStorage.setItem("token", access_token);
+
+    // 2. 사용자 정보 받아오기
+    // axios
+    //   .post(
+    //     "https://kapi.kakao.com/v2/user/me",
+    //     {},
+    //     {
+    //       headers: {
+    //         Authorization: `Bearer ${access_token}`,
+    //       },
+    //     }
+    //   )
+    //   .then((res) => {
+    //     console.log("Kakao 사용자 정보 가져오기 성공!");
+    //     console.log(`닉네임: ${res.data.properties.nickname}`);
+    //     console.log(`이메일: ${res.data.kakao_account.email}`);
+    //     localStorage.setItem("socialUser", res.data.properties.nickname);
+    //   });
+
+    // // 2. 백엔드로 토큰 보내기
+    // axios
+    //   .get("https://wantedclone.com/login/", {
+    //     params: {
+    //       code: access_token,
+    //     },
+    //   }) // 사용자 정보 받기
+    //   .then((res) => {
+    //     // const nickName = res.nickName;
+    //     // const email = res.email;
+
+    //     console.log(res);
+    //   });
+    navigate("/");
+    // }
+    // });
   }, []);
 
   return <></>;
