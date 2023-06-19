db91b87d0e310f0cda8c3c63a651710ed38ec36a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-06-20 02:55:26 +0900

    ♻️ [refactor]: KakaoCallback.js

--- src/KakaoCallback.js
+++ src/KakaoCallback.js
@@ -2,79 +2,16 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-import { REDIRECT_URI } from "./OAuth";
-
 const KakaoCallback = () => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code"); // 인가코드 추출
-  const grant_type = "authorization_code";
-  const client_id = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
-    // 1. 인가코드 전달하여 토큰 발급받기
-    axios
-      .get("https://wantedclone.com/login/", {
-        params: {
-          code: code,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-      });
-
-    // console.log(code);
-    // axios
-    //   .post(
-    //     `https://kauth.kakao.com/oauth/token?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${REDIRECT_URI}&code=${code}`,
-    //     {
-    //       headers: {
-    //         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-    //       },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     const access_token = res.data.access_token;
-
-    //     console.log(access_token);
-
-    //     if (access_token) {
-    //       // 토큰 저장
-    //       localStorage.setItem("token", access_token);
-
-    // 2. 사용자 정보 받아오기
-    // axios
-    //   .post(
-    //     "https://kapi.kakao.com/v2/user/me",
-    //     {},
-    //     {
-    //       headers: {
-    //         Authorization: `Bearer ${access_token}`,
-    //       },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     console.log("Kakao 사용자 정보 가져오기 성공!");
-    //     console.log(`닉네임: ${res.data.properties.nickname}`);
-    //     console.log(`이메일: ${res.data.kakao_account.email}`);
-    //     localStorage.setItem("socialUser", res.data.properties.nickname);
-    //   });
-
-    // // 2. 백엔드로 토큰 보내기
-    // axios
-    //   .get("https://wantedclone.com/login/", {
-    //     params: {
-    //       code: access_token,
-    //     },
-    //   }) // 사용자 정보 받기
-    //   .then((res) => {
-    //     // const nickName = res.nickName;
-    //     // const email = res.email;
-
-    //     console.log(res);
-    //   });
-    // navigate("/");
-    // }
-    // });
+    // 1. 인가코드 전달
+    const url = `http://127.0.0.1:8000/api/?code=${code}`;
+    axios.get(url).then((res) => {
+      console.log("통신 성공", res);
+    });
   }, []);
 
   return <></>;
--- src/OAuth.js
+++ src/OAuth.js
@@ -1,4 +1,4 @@
-const REST_API_KEY = process.env.REACT_APP_API_KEY;
+const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
 export const REDIRECT_URI = "https://wantedclone.com/auth/kakao/callback";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
